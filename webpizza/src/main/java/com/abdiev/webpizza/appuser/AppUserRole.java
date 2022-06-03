e2f06de43528b195825800ca9a244e3618fe2e45
itsabdiev <73123509+itsabdiev@users.noreply.github.com>
2022-06-03 22:15:42 +0600

    Delete webpizza directory

--- webpizza/src/main/java/com/abdiev/webpizza/appuser/AppUserRole.java
+++
@@ -1,6 +0,0 @@
-package com.abdiev.webpizza.appuser;
-
-public enum AppUserRole {
-    ADMIN,
-    USER
-}
--- webpizza/src/main/java/com/abdiev/webpizza/email/EmailSender.java
+++
@@ -1,6 +0,0 @@
-package com.abdiev.webpizza.email;
-
-public interface EmailSender {
-
-    void send(String to , String email);
-}
